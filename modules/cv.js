dcf53067b22234a6cb7a67ac0b60ce41ba585e5b
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-04-11 22:43:03 +0200

    rettet cv der ikke kunne scrolle, nye favicons med transperant baggrund

--- modules/cv.js
+++ modules/cv.js
@@ -4,8 +4,6 @@ export function delegatenCv() {
   console.log("cvDelegation");
   currPos = 0;
   pos = 0;
-  backToSkip();
-  //document.querySelector(".menu-open-button").addEventListener("click", changeHeading);
 
   document.querySelectorAll("#cv, .cv_punkt, .cv_link, #contact, #person, #me, #portfolio, .desk_home, .person, web, .graphics, .contact_punkt, .me_punkt").forEach((punkt) => {
     punkt.addEventListener("click", reset);
@@ -22,71 +20,54 @@ function reset() {
       block: "start",
     });
     section.style.setProperty("--currPos", 0);
-    //document.querySelector(".cv_headings").textContent = "education";
+    document.querySelector(".education").scrollTo(0, 0);
   });
 }
 function setPosition() {
   console.log("setPosition");
   document.querySelector("#education").addEventListener("click", function () {
     reset();
+    const top = document.querySelector(".education");
     pos = 0;
-    move();
+    move(top);
   });
   document.querySelector("#experience").addEventListener("click", function () {
     reset();
+    const top = document.querySelector(".experience");
     pos = 100;
     console.log(currPos);
-    move();
+    move(top);
   });
   document.querySelector("#tech").addEventListener("click", function () {
     reset();
+    const top = document.querySelector(".tech");
     pos = 200;
-    move();
+    move(top);
   });
   document.querySelector("#software").addEventListener("click", function () {
     reset();
+    const top = document.querySelector(".software");
     pos = 300;
-    move();
+    move(top);
   });
   document.querySelector("#volunteer").addEventListener("click", function () {
     reset();
+    const top = document.querySelector(".volunteer");
     pos = 400;
-    move();
+    move(top);
   });
   document.querySelector("#language").addEventListener("click", function () {
     reset();
+    const top = document.querySelector(".language");
     pos = 500;
-    move();
+    move(top);
   });
 }
 
-function changeHeading() {
-  console.log("changeHeading");
-  document.querySelectorAll(".sub_cv").forEach((sub) => {
-    sub.addEventListener("click", function () {
-      console.log("clicked");
-      const heading = this.getAttribute("id");
-      console.log(heading);
-      //document.querySelector(".cv_headings").style.transitionDuration = "1s";
-      document.querySelector(".cv_headings").classList.remove("fade_in_quick2");
-      document.querySelector(".cv_headings").classList.add("fade_out_quick2");
-      setTimeout(() => {
-        document.querySelector(".cv_headings").textContent = heading;
-        document.querySelector(".cv_headings").classList.remove("fade_out_quick2");
-        document.querySelector(".cv_headings").classList.add("fade_in_quick2");
-      }, 300);
-    });
-  });
-}
-
-function backToSkip() {
-  console.log("BackToSkip");
-  /*  document.querySelector(".side").addEventListener("click", skip); */
-}
-
-function move() {
+function move(top) {
   currPos = "-" + pos;
   document.querySelectorAll(".grid_section").forEach((section) => {
     section.style.setProperty("--currPos", currPos);
   });
+  top.scrollTo(0, 0);
 }
